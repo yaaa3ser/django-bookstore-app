aecb0e0f79e0a8f2a683d3298d84db736ff3cac4
yaaa3ser <yasserissa1411@gmail.com>
2023-09-27 17:50:09 +0300

    added pages app, templates and the first page, a static homepage

--- bookstore_management/settings.py
+++ bookstore_management/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local
-    'accounts'
+    'accounts',
+    'pages',
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'bookstore_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
