cfdc60d960c8ba9ecb33956fb745d0eb634f6490
yaaa3ser <yasserissa1411@gmail.com>
2023-10-01 10:33:10 +0300

    used django builtin login and logout

--- bookstore_management/settings.py
+++ bookstore_management/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
